19f14e3f909b774f77b3ed4a6b13402b76563b34
LangqingZou <langqingskye@gmail.com>
2021-01-02 22:42:04 -0500

    GuestConn comments add

--- src/connect/GuestConn.java
+++ src/connect/GuestConn.java
@@ -1,9 +1,17 @@
 package connect;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+/**
+ * The GuestConn program implements an guest connection, includes inserting a guest
+ * and checking email of a guest.
+ * 
+ * @author: Langqing Zou
+ * @version: V1.0
+ * @since: 2021-01-2
+ **/
 
 public class GuestConn extends PersonConn{
 	private String sql;
@@ -11,12 +19,23 @@ public class GuestConn extends PersonConn{
 	private PreparedStatement preparedStatement;
 	private ResultSet resultSet;
 	
+	/**
+	 * This is the constructor which creates a guest connection.
+	 * @param DBConnect
+	 * @return Nothing.
+	 **/
 	public GuestConn(DBConnect dbConnect) {
 		super(dbConnect);
 		db = dbConnect.getConnection();
 	}
 	
-	//insert a new guest, return true if successfully, false otherwise
+	/**
+	 * This function inserts a new guest to guest table in database.
+	 * It returns true if inserting successfully and false otherwise.
+	 * @param email The email of the new guest
+	 * @return Boolean 
+	 * @exception Exception e on inserting a new guest to guest table in database error.
+	 **/
 	public boolean insertGuest(String email) {
 		try {
 			sql = "insert into guest(email) values (?)";
@@ -24,8 +43,6 @@ public class GuestConn extends PersonConn{
 			preparedStatement.setString(1, email);
 			int i =preparedStatement.executeUpdate();
 			if(i>0) {
-//				guest.setPid(resultSet.getInt(1));
-//				guest.setGid(resultSet.getInt(2));
 				return true;
 			}
 		} catch (SQLException e) {
@@ -35,8 +52,13 @@ public class GuestConn extends PersonConn{
 		return false;
 	}
 
-	//return false if email already exist, return true otherwise.
-	
+	/**
+	 * This function check whether the email already exist on guest table or not.
+	 * It returns false if the email already exist on guest table, true otherwise.
+	 * @param email The email of the guest
+	 * @return Boolean 
+	 * @exception Exception e on checking guest email error.
+	 **/
 	public boolean checkEmail(String email) {
 		try {
 			sql = "select * from guest where email = ?";
