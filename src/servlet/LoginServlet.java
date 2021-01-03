d757228b4b4dce33648944764f7ceb7ffd3f8e41
LangqingZou <langqingskye@gmail.com>
2021-01-03 00:02:07 -0500

    LoginServlet comments add

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -1,5 +1,4 @@
 package servlet;
-
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -11,15 +10,34 @@ import javax.servlet.http.HttpSession;
 import connect.DBConnect;
 import connect.PersonConn;
 
+/**
+ * The LoginServlet program receives and processes the data from the login.jsp front end.
+ * 
+ * @author: Langqing Zou
+ * @version: V1.0
+ * @since: 2021-01-2
+ **/
 
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet{
 	@Override
+	/**
+	 * This function receives the request information of HTTP.
+	 * @param request Request from HTTP
+	 * @param response Response to HTTP
+	 * @return Nothing.
+	 **/
 	public void doGet(HttpServletRequest request,HttpServletResponse response)throws IOException,ServletException{
 		doPost(request, response);
 	}
 	
 	@Override
+	/**
+	 * This function receives the request to HTTP to obtain the infomation.
+	 * @param request Request from HTTP
+	 * @param response Response to HTTP
+	 * @return Nothing.
+	 **/
 	public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
